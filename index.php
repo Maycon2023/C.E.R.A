b1c5aed0f72a9e303ec14097b67c70fa8f097901
Maycon2023 <127303829+Maycon2023@users.noreply.github.com>
2023-11-09 15:11:11 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ com o banco
 de dados*/
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = ""; //func2c ou func2d
+$database = "func2c"; //func2c ou func2d
 $username = "root";
 $password = "";
 
@@ -23,24 +23,42 @@ $id = $_POST["id"];
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
 $botao = $_POST["botao"];
+$pesquisa = $_POST["pesquisa"];
 
 if(empty($botao)){
 
 }else if($botao == "Cadastrar"){
     $sql = "INSERT INTO funcionarios 
     (id, nome, cpf) VALUES('','$nome', '$cpf')";
+}else if($botao == "Excluir"){
+    $sql = "DELETE FROM funcionarios WHERE id = '$id'";
+}else if($botao == "Recuperar"){
+    $sql_mostra_cad = "SELECT * FROM funcionarios WHERE nome like '%$pesquiesa%'";
 }
 
-// aqui vou tratar erros das operaçoes C.E.R.A
+//aqui vou tratar erros nas operações C.E.R.A
 if(!empty($sql)){
-    IF(mysqli_query($conexao, $sql)){
-        echo "Operação bem sucedida!!";
+    if(mysqli_query($conexao, $sql)){
+        echo "Operação realizada com sucesso";
     }else{
         echo "Houve um erro na operação: <br />";
-        echo mysqli_error($conexao);
+        echo  mysqli_error($conexao);
     }
 }
-// echo $id." ".$nome." ".$cpf." ".$botao;
+
+$selecionado = $_GET["id"];
+
+if(!empty($selecionado)){
+    $sql_selecionado = "SELECT * FROM funcionarios
+                        WHERE id = $selecionado";
+    $resultado = mysqli_query($conexao, $sql_selecionado); 
+         
+    while($linha = mysqli_fetch_assoc($resultado)){
+        $id = $linha["id"];
+        $nome = $linha["nome"];
+        $cpf = $linha["cpf"];
+    }                      
+}
 
 
 
@@ -49,13 +67,44 @@ if(!empty($sql)){
     <body>
     <form name = "func" method = "post" >
         <label>ID</label>
-        <input type ="text" name = "id" /><br />
+        <input type ="text" name = "id" value="<?php echo $id; ?>"/><br />
         <label>Nome</label>
-        <input type ="text" name = "nome" /><br />
+        <input type ="text" name = "nome" value="<?php echo $nome; ?>"/><br />
         <label>CPF</label>
-        <input type ="text" name = "cpf" /><br />
-        <input type ="submit" name = "botao" value = "cadastrar" />
-        <input type ="reset" name = "botao" value = "cancelar" />
+        <input type ="text" name = "cpf" value="<?php echo $cpf; ?>"/><br />
+        <input type ="submit" name = "botao" value = "Cadastrar" />
+        <input type ="submit" name = "botao" value = "Excluir" />
+        <br />
+        <input type ="text" name = "pesquisa" />
+        <input type ="submit" name = "botao" value = "Recuperar" />
     </form>
+    <table>
+        <tr>
+            <td>-</td>
+            <td>ID</td>
+            <td>Nome</td>
+            <td>CPF</td>
+        </tr>
+        <?php
+        if(empty($pesquisa)){
+            $sql_mostra_cad = "SELECT * FROM fucionarios ORDER BY id desc limit 0,10";
+        }
+         
+         $resultado = mysqli_query($conexao, $sql_mostra_cad); 
+         
+         while($linha = mysqli_fetch_assoc($resultado)){
+            echo "
+            <tr>
+                <td>
+                <a href='?id=".$linha["id"]."'>Selecionar</a>
+                </td>
+                <td>".$linha["id"]."</td>
+                <td>".$linha["nome"]."</td>
+                <td>".$linha["cpf"]."</td>
+            </tr>
+            ";
+         }
+        ?>
+    </table>
     </body>
 </html>
